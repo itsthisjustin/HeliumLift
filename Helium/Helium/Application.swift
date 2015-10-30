d06e8c140cf322311c9209dff1a8ff010d6a8316
reemik <lee@reemik.com>
2015-10-30 11:41:59 +0900

    Changed ShortCuts.
    [Access shortcuts in windows]
    Lowcasestring -> keyCode
    
    - Cmd+L : Open URL
    - Cmd+Y : Hide
    - Cmd '+' : Zoom in
    - Cmd '-' : Zoom Out
    - Cmd+[ : Go Back
    - Cmd+] : Go Forward

--- Helium/Helium/Application.swift
+++ Helium/Helium/Application.swift
@@ -13,46 +13,50 @@ class Application: NSApplication {
     override func sendEvent(event: NSEvent) {
         if event.type == NSEventType.KeyDown {
             if (event.modifierFlags.intersect(NSEventModifierFlags.DeviceIndependentModifierFlagsMask) == NSEventModifierFlags.CommandKeyMask) {
-                switch event.charactersIgnoringModifiers!.lowercaseString {
-                case "x":
-                    if NSApp.sendAction(Selector("cut:"), to:nil, from:self) { return }
-                case "c":
-                    if NSApp.sendAction(Selector("copy:"), to:nil, from:self) { return }
-                case "v":
-                    if NSApp.sendAction(Selector("paste:"), to:nil, from:self) { return }
-                case "z":
-                    if NSApp.sendAction(Selector("undo:"), to:nil, from:self) { return }
-                case "a":
+                
+                let nController = ((NSApplication.sharedApplication().windows.first! as NSWindow).contentViewController as! WebViewController)
+                
+                //switch event.charactersIgnoringModifiers!.lowercaseString {
+                switch event.keyCode {
+                case 0: // a
                     if NSApp.sendAction(Selector("selectAll:"), to:nil, from:self) { return }
-                case "q":
+                case 6: // z
+                    if NSApp.sendAction(Selector("undo:"), to:nil, from:self) { return }
+                case 7: // x
+                    if NSApp.sendAction(Selector("cut:"), to:nil, from:self) { return }
+                case 8: // c
+                    if NSApp.sendAction(Selector("copy:"), to:nil, from:self) { return }
+                case 9: // v
+                    if NSApp.sendAction(Selector("paste:"), to:nil, from:self) { return }
+                case 12: // q
                     NSApplication.sharedApplication().terminate(self)
-                case "t":
+                case 16: // y
+                    let nWindow = (NSApplication.sharedApplication().windows.first! as NSWindow)
+                    if (nWindow.visible) { nWindow.setIsVisible(false); return }
+                    else { nWindow.setIsVisible(true); return }
+                case 17: // t
                     let appDelegate = NSApplication.sharedApplication().delegate as! AppDelegate
                     if(appDelegate.translucent) { appDelegate.didDisableTranslucency() }
                     else { appDelegate.didEnableTranslucency(); }
                     return
-                case "y":
-                    let nWindow = (NSApplication.sharedApplication().windows.first! as NSWindow)
-                    if (nWindow.visible) { nWindow.setIsVisible(false); return }
-                    else { nWindow.setIsVisible(true); return }
+                case 24: // +
+                    nController.zoomIn()
+                    return
+                case 27: // -
+                    nController.zoomOut()
+                    return
+                case 30: // ]
+                    nController.webView.goForward()
+                    return
+                case 33: // [
+                    nController.webView.goBack()
+                    return
+                case 37: // l
+                    (NSApplication.sharedApplication().delegate as! AppDelegate).didRequestLocation()
+                    return
                 default:
-                    let nController = ((NSApplication.sharedApplication().windows.first! as NSWindow).contentViewController as! WebViewController)
-                    switch(event.keyCode) {
-                    case 24: // '+'
-                        nController.zoomIn()
-                        return
-                    case 27: // '-'
-                        nController.zoomOut()
-                        return
-                    case 30: // ']'
-                        nController.webView.goForward()
-                        return
-                    case 33: // '['
-                        nController.webView.goBack()
-                        return
-                    default:
-                        break
-                    }
+                    break
+                
                 }
             }
             else if (event.modifierFlags.intersect(NSEventModifierFlags.DeviceIndependentModifierFlagsMask) == (NSEventModifierFlags.CommandKeyMask.union(NSEventModifierFlags.ShiftKeyMask))) {
